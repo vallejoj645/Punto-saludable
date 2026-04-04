9f12575af59b05b04b9e5209afb117648e90a0d5
vallejoj645 <96347258+vallejoj645@users.noreply.github.com>
2026-04-04 15:37:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -108,8 +108,7 @@ class Usuario(UserMixin, db.Model):
 
 class Mesa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    numero = db.Column(db.Integer, nullable=True, unique=True)
-    nombre_personalizado = db.Column(db.String(40), nullable=True)
+    numero = db.Column(db.Integer, nullable=Flase, unique=True)
     capacidad = db.Column(db.Integer, default=4)
     activa = db.Column(db.Boolean, default=True)
 
